9b8807919ff6dc18c0f9dff3f3dcdadfb4117e59
zacdogwater <zacsengines@gmail.com>
2026-01-27 09:27:48 -0700

    better installer

--- install.sh
+++ install.sh
@@ -7,10 +7,9 @@ mkdir -p ~/.config/nvim
 ln -s $root/.config/hypr          ~/.config
 ln -s $root/.config/alacritty     ~/.config
 ln -s $root/.config/waybar        ~/.config
-ln -s $root/.config/wofi          ~/.config
-ln -s $root/.config/btop          ~/.config
 ln -s $root/.config/nvim/init.lua ~/.config/nvim
 ln -s $root/.bashrc               ~
 ln -s $root/.bash_profile         ~
-sudo ln -s $root/systemd/getty@tty1.service /etc/systemd/system/getty@tty1.service
+sudo cp $root/systemd/getty@tty1.service /etc/systemd/system/getty@tty1.service
+sudo sed -i s/USER/$USER /etc/systemd/system/getty@tty1.service
 
