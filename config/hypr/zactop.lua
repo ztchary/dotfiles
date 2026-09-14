d965afe1faf0e6593b82d118ee41086cddbc0f41
ztchary <zacsengines@gmail.com>
2026-09-14 07:29:52 +0000

    new laptop is so tuff

--- config/hypr/zactop.lua
+++ config/hypr/zactop.lua
@@ -10,6 +10,19 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"))
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"))
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"))
 
+local rot = 0
+
+local function toggle_rotation()
+	rot = (rot + 1) % 4
+	hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = 1, transform = rot})
+	hl.config({input = {
+		touchdevice = { transform = rot },
+		tablet = { transform = rot }
+	}})
+end
+
+hl.bind("SUPER + SHIFT + R", toggle_rotation)
+
 hl.monitor({
 	output = "",
 	mode = "preferred",
@@ -17,11 +30,46 @@ hl.monitor({
 	scale = "1"
 })
 
+function hyprgrass()
+	hl.config({
+		plugin = {
+			hyprgrass = {
+				sensitivity = 4.0,
+				long_press_delay = 400,
+				resize_on_border_long_press = true,
+				edge_margin = 10
+			}
+		}
+	})
+	hl.plugin.hyprgrass.gesture({
+		pattern = {kind = "swipe", fingers = 2, direction = "down"},
+		action = "close"
+	})
+	hl.plugin.hyprgrass.gesture({
+		pattern = {kind = "swipe", fingers = 2, direction = "horizontal"},
+		action = "workspace",
+		scale = 4
+	})
+	hl.plugin.hyprgrass.bind({
+		pattern = {kind = "longpress", fingers = 2},
+		action = hl.dsp.window.fullscreen({ action = "toggle" })
+	})
+	hl.plugin.hyprgrass.bind({
+		pattern = {kind = "swipe", fingers = 2, direction = "up"},
+		action = hl.dsp.exec_cmd("pkill wofi || wofi")
+	})
+	hl.plugin.hyprgrass.bind({
+		pattern = {kind = "longpress", fingers = 3},
+		action = toggle_rotation
+	})
+end
+
 return {
 	hidecursor = true,
 	start = function()
 		hl.exec_cmd("waybar")
 		hl.exec_cmd("alacritty")
+		hl.exec_cmd("hyprpm reload && hyprctl dispatch hyprgrass")
 	end
 }
 
